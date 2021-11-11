88ec2118f3e9843b5b9c2da9268ffcf973fef733
saix511 <saadirfan41@gmail.com>
2021-11-11 20:08:14 +0500

    source graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+var baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8888'
+    : 'https://serverless-virtual-lolly.netlify.app'
 module.exports = {
   siteMetadata: {
     siteUrl: 'https://www.yourdomain.tld',
@@ -10,6 +14,17 @@ module.exports = {
         isTSX: true, // defaults to false
         jsxPragma: `jsx`, // defaults to "React"
         allExtensions: true, // defaults to false
+      },
+    },
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: 'LOLLIES',
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: 'Lollies',
+        // Url to query from
+        url: `${baseUrl}/.netlify/functions/lolly`,
       },
     },
     'gatsby-plugin-image',
